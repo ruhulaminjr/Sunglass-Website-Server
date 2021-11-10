0d03a8c4221781da04bb19026a2795b4e7354db2
ruhulaminjr <ruhultuber@gmail.com>
2021-11-10 22:18:30 +0600

    added user added

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const { MongoClient } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+// firebase token verify setup
+const admin = require("firebase-admin");
+
+const serviceAccount = JSON.parse(process.env.FIREBASEAUTH);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+async function verifyUserToken(req, res, next) {
+  if (req.headers?.authorization.startsWith("Bearer ")) {
+    const userToken = req.headers.authorization.split(" ")[1];
+    const decodedUser = await admin.auth().verifyIdToken(userToken);
+    req.decodedEmail = decodedUser.email;
+  }
+  next();
+}
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -13,6 +29,25 @@ const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+// mongodb connect and server api
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("sunglassDb");
+    const usersCollection = database.collection("users");
+    app.post("/adduser", verifyUserToken, async (req, res) => {
+      const newUser = req.body;
+      if (req.decodedEmail === req.body.email) {
+        const saveUsers = await usersCollection.insertOne(newUser);
+        res.send(saveUsers);
+      } else {
+        res.send(401);
+      }
+    });
+  } finally {
+  }
+}
+run().catch(console.error);
 
 // server api
 app.get("/", (req, res) => {
